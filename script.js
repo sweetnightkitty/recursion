6dd6ccb79e1a65dd798567215fb876c2f239869b
SaraToth <saratoth101@gmail.com>
2024-09-25 18:30:22 +0900

    Simplify the code

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
 function fibs(place) {
     let num1 = 0;
     let num2 = 1;
-    const fibSequence = []
+    const fibSequence = [0, 1]
+    if(place < 1) return [];
     if(place == 1) {
-        fibSequence.push(0);
-    } else if(place == 2) {
-        fibSequence.push(0, 1);
-    }else if(place > 2) {
-        fibSequence.push(0, 1);
+        fibSequence.pop();
+        return fibSequence;
+    }
+
+    if(place == 2) return fibSequence;
+    
+    if(place > 2) {
         for(let i = place; i > 2 ; i--) {
             const total = num1 + num2;
             fibSequence.push(total);
             num1 = num2;
             num2 = total;
         }
-    }
-    return fibSequence;
-}
-
-
+        return fibSequence;
+    }};
+    
 
 function fibsRec(place, num1 = 0, num2 = 1, myArray = [0, 1]) {
-    if(place < 1) {
-        myArray.pop();
-        myArray.pop();
-        return myArray;
-    }
-    
-    if(place == 1) {
-        myArray.pop();
-        return myArray;
-    }
-    if(place == 2) {
-        return myArray;
-    } else if(place > 2) {
+    if(place < 1) return [];
+    if(place == 1) return myArray.pop();
+    if(place == 2) return myArray;
+    if(place > 2){
         const total = num1 + num2;
         myArray.push(total);
         num1 = num2;
         num2 = total;
         fibsRec(place - 1, num1, num2, myArray);
-    }}
+    }
 
+};
 
 
 //Example of the fib sequence
